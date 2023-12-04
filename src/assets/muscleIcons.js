5ee9e3b453fe3d41a3dadf3146424ca61c891a73
Rubxn <rubenbm198@gmail.com>
2023-12-04 23:44:28 +0100

    Cambio images

--- src/assets/muscleIcons.js
+++ src/assets/muscleIcons.js
@@ -1,8 +1,8 @@
 export const muscleIcons = {
-    6: 'src/assets/MuscleIcons/legIconBlue.png',
-    1: 'src/assets/MuscleIcons/chestIconBlue.png',
-    2: 'src/assets/MuscleIcons/backIconBlue.png',
-    3: 'src/assets/MuscleIcons/chestIconBlue.png',
-    4: 'src/assets/MuscleIcons/armIconBlue.png',
-    5: 'src/assets/MuscleIcons/armIconBlue.png',
+    6: './MuscleIcons/legIconBlue.png',
+    1: './MuscleIcons/chestIconBlue.png',
+    2: './MuscleIcons/backIconBlue.png',
+    3: './MuscleIcons/chestIconBlue.png',
+    4: './MuscleIcons/armIconBlue.png',
+    5: './MuscleIcons/armIconBlue.png',
 };
